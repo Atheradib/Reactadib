5aecc00902ddc24923fa2589679d7178890f3f72
Atheradib <adib.rapha@gmail.com>
2021-07-25 11:21:31 +0530

    25/7/21

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,32 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { CourseInfo } from "./components/CourseInfo";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
     
+    
     <div className="App">
-      <center>
-      <h1 style={{color:"green", fontSize:"larger"}}>List Of Employees</h1>
-    <tr>
-    <td><CourseInfo coursename="PDAC" instituteName="pickupbiz" contact="789456123"/></td>
-    <td><CourseInfo coursename="CDAC" instituteName="Faisal Institute" contact="457891265"/></td>
-   <td> <CourseInfo coursename="HTML" instituteName="Ather Institute" contact="4456665" /></td>
-   </tr>
-   <hr />
-   <td>
-    <CourseInfo coursename="PDAC" instituteName="pickupbiz" contact="789456123"/></td>
-    <td><CourseInfo coursename="CDAC" instituteName="Faisal Institute" contact="457891265"/></td>
-    <td> <CourseInfo coursename="HTML" instituteName="Ather Institute" contact="4456665" /></td>
-   <hr/>
-   
-   <td>
-    <CourseInfo coursename="PDAC" instituteName="pickupbiz" contact="789456123"/></td>
-    <td><CourseInfo coursename="CDAC" instituteName="Faisal Institute" contact="457891265"/></td>
-   <td> <CourseInfo coursename="HTML" instituteName="Ather Institute" contact="4456665" /></td>
-   <br/>
-   <br/>
-   </center>
+      <Navigation />
+      
     </div>
     
   );
