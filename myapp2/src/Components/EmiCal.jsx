9e3ee345de90cbd8f8ce3a880f0354426a23e1f3
Atheradib <adib.rapha@gmail.com>
2021-07-28 13:37:22 +0530

    EmiCalc update

--- myapp2/src/Components/EmiCal.jsx
+++ myapp2/src/Components/EmiCal.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Card, Row, Col } from "react-bootstrap";
+import "./EmiCal.css";
 
 export const EmiCal = () => {
   return (
     <div>
-      <Form.Control type="text" />
-      <Form.Control type="text" />
-      <Form.Control type="text" />
-      <Button Variant="primary">Calculator</Button>
+      <Card className="Calc">
+        <Row>
+          <Col md={1}></Col>
+          <Col md={3}>
+            <Form.Control type="text" placeholder="principle Amount" />
+          </Col>
+          <Col md={3}>
+            <Form.Control type="text" placeholder="Number of Year" />
+          </Col>
+          <Col md={3}>
+            <Form.Control type="text" placeholder="Rate of Interest" />
+          </Col>
+          <Col md={2}>
+            <Button Variant="primary">EMI Cal-C</Button>
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 };
