630df44b35a05f28719921428bdfb2b796a054da
Atheradib <adib.rapha@gmail.com>
2021-07-28 18:14:11 +0530

    EmiCalc Completed

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { EmiCal } from "./Components/EmiCal";
+import { Piano } from "./Components/Piano";
+// import { EmiCal } from "./Components/EmiCal";
 // import { ToggleComps } from "./Components/ToggleComps";
 // import { Forms } from "./Components/Forms";
 // import { Form1 } from "./Components/Form1";
@@ -17,7 +18,8 @@ function App() {
       </div>
     </div> */}
       {/* <ToggleComps /> */}
-      <EmiCal />
+      {/* <EmiCal /> */}
+      <Piano />
     </div>
   );
 }
