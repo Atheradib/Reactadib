5b222e3fa81ebc87339617ea9ce75a75dbe0054b
Atheradib <adib.rapha@gmail.com>
2021-07-29 13:16:13 +0530

    Piano

--- myapp2/src/Components/Piano.jsx
+++ myapp2/src/Components/Piano.jsx
@@ -3,11 +3,36 @@ import { Row, Col, Card, Button } from "react-bootstrap";
 import "./Piano.css";
 
 export const Piano = () => {
+  const handlePlay = () => {
+    const audio = new Audio(`Music/${bit}.mp3`);
+    audio.play();
+  };
   return (
     <div>
       <Card>
         <Row>
           <Col md={4}>
+            <h1 className="h11">Low</h1>
+            <Button className="">Sa</Button>
+            <Button className="">Re</Button>
+            <Button className="">Ga</Button>
+            <Button className="">Ma</Button>
+            <Button className="">Pa</Button>
+            <Button className="">Dha</Button>
+            <Button className="">Ni</Button>
+          </Col>
+          <Col md={4}>
+            <h1 className="h12">Medium</h1>
+            <Button className="">Sa</Button>
+            <Button className="">Re</Button>
+            <Button className="">Ga</Button>
+            <Button className="">Ma</Button>
+            <Button className="">Pa</Button>
+            <Button className="">Dha</Button>
+            <Button className="">Ni</Button>
+          </Col>
+          <Col md={4}>
+            <h1 className="h13">High</h1>
             <Button className="">Sa</Button>
             <Button className="">Re</Button>
             <Button className="">Ga</Button>
