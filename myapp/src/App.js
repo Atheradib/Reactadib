331042f8a2b675698ea1a4c76567ca265fb6b15b
Atheradib <adib.rapha@gmail.com>
2021-07-29 13:14:55 +0530

    nokiatue

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
-    
-    
     <div className="App">
       <Navigation />
-      
     </div>
-    
   );
 }
 
