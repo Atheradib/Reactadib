06371ce8d2fb030c463b6c5dbbb312d947a3feb9
Atheradib <adib.rapha@gmail.com>
2021-07-29 13:15:32 +0530

    AudioPlayer

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -1,24 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Piano } from "./Components/Piano";
-// import { EmiCal } from "./Components/EmiCal";
-// import { ToggleComps } from "./Components/ToggleComps";
-// import { Forms } from "./Components/Forms";
-// import { Form1 } from "./Components/Form1";
 
 function App() {
   return (
     <div className="App">
-      {/* <div>
-        <Forms />
-      </div>
-      <hr />
-      <div>
-        <Form1 />
-      </div>
-    </div> */}
-      {/* <ToggleComps /> */}
-      {/* <EmiCal /> */}
       <Piano />
     </div>
   );
